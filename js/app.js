96493f93f6ace7d16e558eec53c9a08911dac773
Pompeu <itacir@hotmail.com>
2015-11-02 17:54:43 -0200

    rxjs auto complete done

--- js/app.js
+++ js/app.js
@@ -1,41 +1,29 @@
-(function(){
-	var ajax =  new XMLHttpRequest();
-	var posts = [];
-  var titles = [];
-  ajax.open('get','articles/data.json',true);
-  ajax.send();
-  ajax.addEventListener('load',function respAjax(){
-   posts = JSON.parse(ajax.response);
-   posts = Object.keys(posts).map(function(key){
-     return posts[key];
-   });
-  },false);
-  var searchInput = document.querySelector('#find-btn');
-  var result = document.querySelector('.result');
+(function () {
+  var q = document.querySelector('#q');
+  var resultList = document.querySelector('#results');
+  var keyups = Rx.Observable.fromEvent(q, 'keyup');
 
-  searchInput.addEventListener("keyup", function(ev) {
-      var valueOfInput = new RegExp(searchInput.value,'ig');
-      var filterposts = [];
-      if(searchInput.value.length){
-        filterposts = posts.filter(function(post, index, arr){
-          return post.title.match(valueOfInput);
-        });
-        //filterposts.forEach(print);
-        
-        function print(data){
-          var a = document.createElement("a");
-          a.href = data.url;
-          a.className = 'search'
-          a.innerHTML = data.title.toLowerCase();
-          var br = document.createElement("br");
-          result.appendChild(br);
-          result.appendChild(a);
-        }
 
-      } 
-  });
-  function log(data){
-    return console.log(data);
-  }
+  keyups.throttle(500)
+    .map(() => q.value)
+    .do(() => q.classList.add('spinner'))
+    .flatMapLatest(() => Rx.DOM.ajax({
+      method: 'GET',
+      url: '../js/data.json',
+      responseType: 'json'
+    }))
+    .do(() => q.classList.remove('spinner'))
+    .map(r => r.response)
+    .map(results => 
+      results
+        .filter(r => { 
+          var re = q.value !== '' ? new RegExp(q.value, 'ig'): /^\w+$/
+          return r.title.match(re)
+         })
+        .reduce((html, result) => 
+      `${html}<div class="result"><a href="${result.url}">${result.title}</a></div>`, '')
+    )
+    .subscribe(resultsHTML => resultList.innerHTML = resultsHTML, 
+      err => console.error(err))
+
 })();
-
