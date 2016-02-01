f6042ea58f4f00eb13d94339ce30ba9673947b62
pompeu <itacir@hotmail.com>
2016-01-31 22:49:28 -0200

    end post elixir vs node v1

---
+++ exemples/functional_list/filter.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const list  = [1,2,3,4,5,6,7,42,319];
+let listOdd  = [];
+let listEven = [];
+
+listOdd     = list.filter(checkOdd);
+listEven    = list.filter(checkEven);
+
+
+function checkEven(n) {
+  return n%2 === 0;
+}
+
+function checkOdd (n) {
+  return n%2 !==0;
+}
+console.log(listOdd);
+console.log(listEven);
+/*
+  389
+  54
+  335
+*/
---
+++ exemples/functional_list/reduce.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const list  = [1,2,3,4,5,6,7,42,319];
+let sum     = 0;
+let sumOdd  = 0;
+let sumEven = 0;
+
+sum     = list.reduce((n, i) => n+i , 0);
+
+sumOdd  = list
+            .filter(n => n%2 === 0)
+            .reduce((n,i) => n+i,0);
+
+sumEven = list
+            .filter(n => n%2 !== 0)
+            .reduce((n,i) => n+i,0);
+
+console.log(sum);
+console.log(sumOdd);
+console.log(sumEven);
+/*
+  389
+  54
+  335
+*/
